fd5c0d628cc1b7dcc42310ed03a5facf621dccbe
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2017-01-31 15:50:05 +0330

    add some controllers

---
+++ src/GoProject.Sample/Controllers/GoController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoProject.Sample.Controllers
+{
+    public class GoController : Controller
+    {
+        
+        public ActionResult PageFlowSample()
+        {
+            return View();
+        }
+
+        public ActionResult SystemDynamicsSample()
+        {
+            return View();
+        }
+
+        public ActionResult PlanogramSample()
+        {
+            return View();
+        }
+
+        public ActionResult DoubleTreeSample()
+        {
+            return View();
+        }
+
+        public ActionResult BpmnEditorSample()
+        {
+            return View();
+        }
+
+        public ActionResult PipesSample()
+        {
+            return View();
+        }
+
+
+    }
+}
