afaaa827bc81bd4afda2b632a2e6b1ab99f598eb
Behzad Khosravifar <Behzad.khosravifar@gmail.com>
2017-02-11 15:48:56 +0330

    Fix some js bugs

--- src/GoProject.Sample/App_Start/BundleConfig.cs
+++ src/GoProject.Sample/App_Start/BundleConfig.cs
@@ -8,32 +8,26 @@ namespace GoProject.Sample
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/go").Include("~/Scripts/GoJs/go.js"));
+            bundles.Add(new ScriptBundle("~/bundles/goJs").Include("~/Scripts/GoJs/go.js"));
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"
-                //,"~/Scripts/jquery-ui-{version}.js"
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/jquery-ui-{version}.js"
                 ));
-            
-            bundles.Add(new ScriptBundle("~/bundles/bpmn").Include(
+
+            bundles.Add(new ScriptBundle("~/bundles/bpmnJs").Include(
                 "~/Scripts/GoJs/DrawCommandHandler.js",
                 "~/Scripts/GoJs/BPMNClasses.js",
                 "~/Scripts/GoJs/BPMN.js"));
 
-            bundles.Add(new StyleBundle("~/Content/go").Include(
-              //  "~/Content/themes/base/jquery-ui.min.css",
-                "~/Content/GoJs/goSamples.css",
+            bundles.Add(new StyleBundle("~/Content/goCss").Include(
+                "~/Content/themes/base/jquery-ui.min.css",
                 "~/Content/GoJs/bpmn.css"));
-
-            bundles.Add(new StyleBundle("~/Content/main").Include("~/Content/GoJs/main.css"));
-
 
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include("~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js","~/Scripts/respond.js"));
-
+            
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.css", "~/Content/site.css"));
         }
     }
